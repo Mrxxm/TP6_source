cd45ed361940e37c7f72cfcb269c521c6d138317
Mrxxm <362190221@qq.com>
2020-08-19 14:31:34 +0800

    ReflectionClass

--- extend/RegisterA.php
+++ extend/RegisterA.php
@@ -3,11 +3,23 @@
 
 class RegisterA
 {
+
+    protected $age = 23;
+    /**
+     * register
+     * @return string
+     */
     public function abc()
     {
         return 'abc';
     }
 
+    /**
+     * 反射
+     * @param $a
+     * @param $b
+     * @return string
+     */
     public function ref($a, $b)
     {
         return $a . $b;
