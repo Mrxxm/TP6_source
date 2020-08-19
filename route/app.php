80843fae0c6b8d9f487ed3c0389e7905c66c3e92
Mrxxm <362190221@qq.com>
2020-08-19 17:41:56 +0800

    Countable

--- route/app.php
+++ route/app.php
@@ -27,6 +27,8 @@ Route::get('rel', 'index/rel');
 Route::get('rel2', 'index/rel2');
 Route::get('container', 'index/container');
 Route::get('containerRef', 'index/containerRef');
+Route::get('countable', 'index/countable');
+
 
 
 
