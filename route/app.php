82184ae001a14800ec95005a57a0c32acab1751c
Mrxxm <362190221@qq.com>
2020-08-19 13:51:25 +0800

    register

--- route/app.php
+++ route/app.php
@@ -21,6 +21,8 @@ Route::get('obj', 'index/obj');
 Route::get('ini', 'index/ini');
 Route::get('yaml', 'index/yaml');
 Route::get('single', 'index/single');
+Route::get('register', 'index/register');
+
 
 
 
