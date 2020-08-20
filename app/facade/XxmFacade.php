1dd9f5fc18e7df9701325c1d75cb3afaada32a71
Mrxxm <362190221@qq.com>
2020-08-20 15:47:33 +0800

    门面

---
+++ app/facade/XxmFacade.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace app\facade;
+
+
+use think\Facade;
+
+class XxmFacade extends Facade
+{
+    public static function getFacadeClass()
+    {
+        return 'app\common\XxmFacade';
+    }
+}
