6a905df47174e325a92e543e26475e3a1cd3ab92
Mrxxm <362190221@qq.com>
2020-08-20 15:52:32 +0800

    完善注释

--- route/app.php
+++ route/app.php
@@ -15,21 +15,21 @@ Route::get('think', function () {
 });
 
 Route::get('hello/:name', 'index/hello');
-Route::get('ali', 'index/ali');
-Route::get('scanner', 'index/scanner');
-Route::get('obj', 'index/obj');
-Route::get('ini', 'index/ini');
-Route::get('yaml', 'index/yaml');
-Route::get('single', 'index/single');
-Route::get('register', 'index/register');
-Route::get('personBuy', 'index/personBuy');
-Route::get('rel', 'index/rel');
-Route::get('rel2', 'index/rel2');
-Route::get('container', 'index/container');
-Route::get('containerRef', 'index/containerRef');
-Route::get('countable', 'index/countable');
-Route::get('container1', 'index/container1');
-Route::get('facade', 'index/facade');
+Route::get('ali', 'index/ali'); // 类自动加载-自定义文件夹类
+Route::get('scanner', 'index/scanner'); // 自定义composer包-手动加载
+Route::get('obj', 'index/obj'); // ArrayAccess
+Route::get('ini', 'index/ini'); // yaconf
+Route::get('yaml', 'index/yaml'); // yaml
+Route::get('single', 'index/single'); // 单例模式
+Route::get('register', 'index/register'); // 注册树模式
+Route::get('personBuy', 'index/personBuy'); // 依赖注入
+Route::get('rel', 'index/rel'); // 反射机制
+Route::get('rel2', 'index/rel2'); // 反射机制-调用方法
+Route::get('container', 'index/container'); // 容器
+Route::get('containerRef', 'index/containerRef'); // 容器-反射
+Route::get('countable', 'index/countable'); // Countable
+Route::get('container1', 'index/container1'); // 容器获取实例
+Route::get('facade', 'index/facade'); // 门面模式
 
 
 
