90e80c6d55151e763bf17c48276f1e6856ce023a
Mrxxm <362190221@qq.com>
2020-08-21 16:26:53 +0800

    provider

--- app/provider.php
+++ app/provider.php
@@ -1,9 +1,11 @@
 <?php
 use app\ExceptionHandle;
 use app\Request;
+use app\xxmProvider;
 
 // 容器Provider定义文件
 return [
     'think\Request'          => Request::class,
     'think\exception\Handle' => ExceptionHandle::class,
+    'xxmProvider'            => xxmProvider::class,
 ];
